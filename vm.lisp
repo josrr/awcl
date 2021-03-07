8700b28379948fe5a3c8d01cbf0aba53d26f04ec
J. M. R. R <josrr@ymail.com>
2021-03-06 22:23:22 -0600

    Implementé la operación cond-jmp

--- vm.lisp
+++ vm.lisp
@@ -246,8 +246,28 @@
         (fetch-word stream)))
   nil)
 
-(def-op-function (vm stream (cjmp #x0A))
-  (declare (ignore vm))
+(def-op-function (vm stream (cond-jmp #x0A))
+  (let* ((opcode (fetch-byte stream))
+         (b (aref (vm-variables vm) (fetch-byte stream)))
+         (a (cond
+              ((> (logand opcode #x80) 0)
+               (aref (vm-variables vm) (fetch-byte stream)))
+              ((> (logand opcode #x40) 0)
+               (fetch-word stream))
+              (t (fetch-byte stream)))))
+    (if (funcall (case (logand opcode #x7)
+                   (0 #'=)
+                   (1 #'/=)
+                   (2 #'>)
+                   (3 #'<)
+                   (4 #'<=)
+                   (5 #'>=)
+                   (otherwise
+                    (warn "cond-jmp-op: invalid condition ~d"
+                          (logand opcode #x7))))
+                 b a)
+        (jmp-op vm stream)
+        (fetch-word stream)))
   nil)
 
 (def-op-function (vm stream (set-pal #x0B))
